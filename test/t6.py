5aee8e7b314c25abf7f2becefb9dfe6e181e9018
chatravinchattong <101159845+chatravinchattong@users.noreply.github.com>
2024-12-09 15:47:15 +0700

    Delete test/t6.py

--- test/t6.py
+++
@@ -1,9 +0,0 @@
-start = int(input("Enter the start number: "))
-end = int(input("Enter the end number: "))
-
-total_sum = 0
-
-for num in range(start,end + 1):
-    total_sum += num
-
-print (f"The sum from {start} to {end} is : {total_sum}")
